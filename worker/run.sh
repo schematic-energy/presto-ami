89b994d5260f198b526b5ebc908375d17467d4fa
Luke VanderHart <luke@vanderhart.net>
2020-08-25 11:54:46 -0400

    more overhead is better due to direct allocations

--- worker/run.sh
+++ worker/run.sh
@@ -16,7 +16,7 @@ export PRESTO_CONFIG_PATH=$2
 cd `dirname $0`
 
 SYSTEM_MEMORY_KB=$(cat /proc/meminfo | grep MemTotal | grep -oe '[0-9]*')
-JVM_MEM=$(($SYSTEM_MEMORY_KB - 4194304))K # Everything minus 4gb
+JVM_MEM=$((($SYSTEM_MEMORY_KB / 5) * 4))K # Everything minus 4gb
 
 echo "export PRESTO_CONFIG_PATH=$PRESTO_CONFIG_PATH" | sudo tee -a /etc/profile > /dev/null
 echo "export ENV=$ENV" | sudo tee -a /etc/profile > /dev/null
